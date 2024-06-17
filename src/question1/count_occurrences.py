a26844d095f6db962d5f37bfd0c879885bc8fb9c
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-17 15:42:22 -0400

    Update count_occurrences.py

--- src/question1/count_occurrences.py
+++ src/question1/count_occurrences.py
@@ -4,20 +4,3 @@ def count_occurrences(arr, target):
         if len(arr) == 0
         else (1 if arr[0] == target else 0) + count_occurrences(arr[1:], target)
     )
-
-
-test_array = [1, 2, 3, 4, 2, 2, 5, 2, 6]
-target_element = 2
-print(f"Test Case 1: The element {target_element} appears {count_occurrences(test_array, target_element)} times in the array.")
-
-
-test_array = [1, 3, 4, 5, 6]
-target_element = 3
-
-print(f"Test Case 2: The element {target_element} appears {count_occurrences(test_array, target_element)} times in the array.")
-
-
-test_array = []
-target_element = 4
-
-print(f"Test Case 3: The element {target_element} appears {count_occurrences(test_array, target_element)} times in the array.")
