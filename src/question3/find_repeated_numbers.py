3ecf26d33d6e202af3d678fe63ed402a25ea561a
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-17 15:45:34 -0400

    Update find_repeated_numbers.py

--- src/question3/find_repeated_numbers.py
+++ src/question3/find_repeated_numbers.py
@@ -1,11 +1,11 @@
-def find_repeated_numbers(B):
-    B.sort()
+def find_repeated_numbers(b):
+    b.sort()
     repeated_numbers = []
 
     repeated_numbers = [
-        B[i]
-        for i in range(1, len(B))
-        if B[i] == B[i - 1] and B[i] not in repeated_numbers
+        b[i]
+        for i in range(1, len(b))
+        if b[i] == b[i - 1] and b[i] not in repeated_numbers
     ]
 
     return repeated_numbers
